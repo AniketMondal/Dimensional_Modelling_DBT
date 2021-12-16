4c53ee73e75463338580cd980d37c1a55e811c10
AniketMondal <am743268@gmail.com>
2021-12-16 05:32:18 +0530

    added date dimension

---
+++ models/staging/date.sql
@@ -0,0 +1,16 @@
+with dates as
+        (select distinct(cast(date as date)) as date from {{ source('raw_tbl', 'COVID_19_INDONESIA_ANIKET_MONDAL') }}),
+     final as
+        (select concat(year(date),lpad(cast(month(date) as varchar),2,'0'),lpad(cast(day(date) as varchar),2,'0')) as date_id,
+        date,
+        day(date) as day,
+        dayname(date) as day_name,
+        month(date) as month,
+        monthname(date) as month_name,
+        year(date) as year,
+        weekiso(date) as iso_week,
+        yearofweekiso(date) as iso_year_of_week,
+        quarter(date) as quarter_of_year
+        from dates order by date)
+
+select * from final
